291183a0b7179812906e101c2284067e9fba5c76
estefancivera <estefan.civera@gmail.com>
2019-03-04 16:54:28 +0100

    Completamento anagrafica contatti

--- Contacts.php
+++ Contacts.php
@@ -4,19 +4,19 @@
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
+ * Portions created by Algoma CRM info@algoma.it.
  * All Rights Reserved.
  ************************************************************************************/
 $languageStrings = array(
 	'Contacts'                     => 'Contatti'                    , 
 	'SINGLE_Contacts'              => 'Contatto'                    , 
-	'LBL_ADD_RECORD'               => 'Add Contact'                 , 
-	'LBL_RECORDS_LIST'             => 'Contacts List'               , 
+	'LBL_ADD_RECORD'               => 'Nuovo contatto'                 , 
+	'LBL_RECORDS_LIST'             => 'Lista contatti'               , 
 	'LBL_CONTACT_INFORMATION'      => 'Informazioni Contatto'       , 
 	'LBL_CUSTOMER_PORTAL_INFORMATION' => 'Informazioni Portale Clienti', 
-	'LBL_IMAGE_INFORMATION'        => 'Informazioni Immagine Contatto:', 
-	'LBL_COPY_OTHER_ADDRESS'       => 'Copia Altro indirizzo'          , // TODO: Review
-	'LBL_COPY_MAILING_ADDRESS'     => 'Copia Indirizzo postale'        , // TODO: Review
+	'LBL_IMAGE_INFORMATION'        => 'Informazioni Immagine Contatto', 
+	'LBL_COPY_OTHER_ADDRESS'       => 'Copia Altro indirizzo'          ,
+	'LBL_COPY_MAILING_ADDRESS'     => 'Copia Indirizzo postale'        ,
 	'Office Phone'                 => 'Telefono Ufficio'            , 
 	'Home Phone'                   => 'Telefono Casa'               , 
 	'Title'                        => 'Titolo'                      , 
@@ -33,13 +33,13 @@ $languageStrings = array(
 	'Mailing State'                => 'Provincia (spedizione)'      , 
 	'Mailing Zip'                  => 'CAP (spedizione)'            , 
 	'Mailing Country'              => 'Stato (spedizione)'          , 
-	'Mailing Po Box'               => 'Casella postale di mailing'            , 
+	'Mailing Po Box'               => 'Casella postale (spedizione)', 
 	'Other Street'                 => 'Altra Via'                   , 
 	'Other City'                   => 'Altra Citt&agrave;'          , 
 	'Other State'                  => 'Altra Provincia'             , 
 	'Other Zip'                    => 'Altro CAP'                   , 
 	'Other Country'                => 'Altro Stato'                 , 
-	'Other Po Box'                 => 'Other P.O. Box'              , 
+	'Other Po Box'                 => 'Altra casella postale'       , 
 	'Contact Image'                => 'Immagine Contatto'           , 
 	'Mr.'                          => 'Sig.'                        , 
 	'Ms.'                          => 'Sig.ra'                      , 
@@ -48,13 +48,22 @@ $languageStrings = array(
 	'Prof.'                        => 'Prof.'                       , 
 	'User List'                    => 'Lista Utente'                , 
 	'Contact Id' => 'Contatto Id',
-    'Support Start Date'           => 'Supporto Data di inizio'     ,
-    'Support End Date'             => 'Data di supporto End'        ,
+    'Support Start Date'           => 'Data inizio supporto'     ,
+    'Support End Date'             => 'Data termine supporto'        ,
 	'LBL_TRANSACTIONS' => 'Transazioni',
 
   'Other Phone' => 'Telefono Secondario',
   'Email' => 'Email Principale',
   'Secondary Email' => 'E-Mail Secondaria',
+  
+  
+  // Blocks
+	'LBL_DESCRIPTION_INFORMATION' => 'Altre informazioni',
+	'LBL_ADDRESS_INFORMATION'=> 'Informazioni localizzazione',
+	'LBL_CONTACT_INFORMATION' => 'Informazioni principali',
+	'LBL_CUSTOMER_PORTAL_INFORMATION' => 'Informazioni portale clienti',
+	'LBL_IMAGE_INFORMATION' => 'Profilo',
+
 
 );
 $jsLanguageStrings = array(
