24d61eca2fc278770081800aaca897d93f7c1137
estefancivera <estefan.civera@gmail.com>
2019-03-05 07:25:35 +0100

    Aggiornamento modulo Accounts

--- Accounts.php
+++ Accounts.php
@@ -5,12 +5,13 @@
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
+ * Portions created by AlgomaCRM www.algoma.it.
  * All Rights Reserved.
  ************************************************************************************/
 $languageStrings = array(
 	'Accounts'                     => 'Aziende'                     , 
 	'SINGLE_Accounts'              => 'Azienda'                     , 
-	'LBL_ADD_RECORD'               => 'Add Organization'            , 
+	'LBL_ADD_RECORD'               => 'Nuova azienda'            	, 
 	'LBL_RECORDS_LIST'             => 'Lista Aziende'               , 
 	'LBL_ACCOUNT_INFORMATION'      => 'Informazioni Azienda'        , 
 	'LBL_SHOW_ACCOUNT_HIERARCHY'   => 'Mostra gerarchia Aziende'    , 
@@ -22,7 +23,7 @@ $languageStrings = array(
 	'Member Of'                    => 'Membro di'                   , 
 	'Employees'                    => 'Impiegati'                   , 
 	'Ownership'                    => 'Propriet&agrave;'            , 
-	'SIC Code'                     => 'Codice SIC'                  , 
+	'SIC Code'                     => 'Codice S.I.C'                , 
 	'Other Email'                  => 'Altra Email'                 , 
 	'Analyst'                      => 'Analista'                    , 
 	'Competitor'                   => 'Concorrente'                 , 
@@ -32,25 +33,24 @@ $languageStrings = array(
 	'Press'                        => 'Stampa'                      , 
 	'Prospect'                     => 'Potenziale'                  , 
 	'Reseller'                     => 'Rivenditore'                 , 
-        'Type'                         => 'Tipo'                        , 
+    'Type'                         => 'Tipo'                        , 
 	'LBL_START_DATE'               => 'Data Inizio'                 , 
 	'LBL_END_DATE'                 => 'Data Fine'                   , 
-	'LBL_DUPLICATES_EXIST'         => 'Organization Name already exists', 
-	'LBL_COPY_SHIPPING_ADDRESS'    => 'Indirizzo Copy spedizione'       , // TODO: Review
-	'LBL_COPY_BILLING_ADDRESS'     => 'Copia indirizzo di fatturazione'        , // TODO: Review
+	'LBL_DUPLICATES_EXIST'         => 'Azienda gi&agrave; esistente', 
+	'LBL_COPY_SHIPPING_ADDRESS'    => 'Copia indirizzo di spedizione'       , 
+	'LBL_COPY_BILLING_ADDRESS'     => 'Copia indirizzo di fatturazione'        ,
     'LBL_IMAGE_INFORMATION' => 'Picture Profile',
-    'Organization Image' => 'Organizzazione Immagine',
+    'Organization Image' => 'Logo aziendale',
 
   'Other Phone' => 'Telefono Secondario',
   'Phone' => 'Telefono Principale',
   'Email' => 'Email Principale',
 
 );
+
 $jsLanguageStrings = array(
-	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Are you sure you want to Delete?', 
-	'LBL_DELETE_CONFIRMATION'      => 'Deleting this Organization will remove its related Opportunities & Quotes. Are you sure you want to delete this Organization?', 
-	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this Organization will remove its related Opportunities & Quotes. Are you sure you want to delete this Organization?', 
-
-  'JS_DUPLICATE_CREATION_CONFIRMATION' => 'Organizzazione Nome Esiste già.Vuoi creare un record duplicato?',
-
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Cancellare l\'azienda?', 
+	'LBL_DELETE_CONFIRMATION'      => 'Cancellando l\'azienda i dati correlati (preventi e opportunit&agrave; verranno rimossi. Procedere?', 
+	'LBL_MASS_DELETE_CONFIRMATION' => 'Cancellando l\'azienda i dati correlati (preventi e opportunit&agrave; verranno rimossi. Procedere?',
+	'JS_DUPLICATE_CREATION_CONFIRMATION' => 'Organizzazione gi&agrave; esistente, vuoi creare un record duplicato?',
 );
